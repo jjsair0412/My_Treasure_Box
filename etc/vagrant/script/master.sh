08ec5af9d4cefd5c787a7d292ead445aaaae9a1e
Jinseong Ju <jjsair0412@naver.com>
2023-03-13 16:37:07 +0900

    master.sh 수정

--- etc/vagrant/script/master.sh
+++ etc/vagrant/script/master.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-$OUPUT_FILE=/home/vagrant/join/join_command.sh
-rm -rf /vagrant/join/join.sh
 echo 'init start ..'
-sudo kubeadm init --apiserver-advertise-address=192.168.50.10
+sudo kubeadm init --apiserver-advertise-address=192.168.50.10 --pod-network-cidr=192.168.0.0/16
 
 echo 'token create ..'
-mkdir join
+rm -rf /join/join_command.sh
 sudo kubeadm token create --print-join-command > /home/vagrant/join/join_command.sh
 
 chmod +x /home/vagrant/join/join_command.sh
@@ -14,6 +12,7 @@ chmod +x /home/vagrant/join/join_command.sh
 echo 'print join command file location ..'
 ls
 
+su vagrant # user 변경
 mkdir -p $HOME/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
