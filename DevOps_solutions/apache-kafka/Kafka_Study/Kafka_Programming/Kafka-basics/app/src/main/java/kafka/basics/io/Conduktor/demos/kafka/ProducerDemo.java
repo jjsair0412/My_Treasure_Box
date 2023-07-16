2d42f68061680466ee8e7b8206d7f755d1cbfe4b
jinseong <jjsair0412@naver.com>
2023-07-17 00:23:31 +0900

    start Kafka programming!

---
+++ DevOps_solutions/apache-kafka/Kafka_Study/Kafka_Programming/Kafka-basics/app/src/main/java/kafka/basics/io/Conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,7 @@
+package main.java.kafka.basics.io.Conduktor.demos.kafka;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("hello Kafka !!!!");
+    }
+}
