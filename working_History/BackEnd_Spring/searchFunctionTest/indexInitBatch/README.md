9dcd52d684b1acdf309ffea6cf4694c05b990164
jjsair0412 <jjsair0412@naver.com>
2023-09-07 15:52:57 +0900

    elasticsearch 활용한 검색기능 테스트 init

---
+++ working_History/BackEnd_Spring/searchFunctionTest/indexInitBatch/README.md
@@ -0,0 +1,2 @@
+# spring batch server
+elastic search에 데이터를 주기적으로 넣어주는 배치 api
---
+++ working_History/BackEnd_Spring/searchFunctionTest/indexInitBatch/src/test/java/com/example/indexinitbatch/IndexInitBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indexinitbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexInitBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
